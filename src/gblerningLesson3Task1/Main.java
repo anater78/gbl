e2a63b08d6ad49cb622323346f4377f17d82cbca
anater <anater78@gmail.com>
2019-10-14 13:13:22 +0300

    add lesson 3 task1

--- src/gblerningLesson3Task1/Main.java
+++ src/gblerningLesson3Task1/Main.java
@@ -1,11 +1,45 @@
 package gblerningLesson3Task1;
 
+
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("HW lesson3");
 
+        List <String> al = new ArrayList<>();
 
+        Map<String,Integer> ml = new TreeMap<>();
+
+        al.add("Yellow");
+        al.add("Green");
+        al.add("Blue");
+        al.add("Brown");
+        al.add("White");
+        al.add("Red");
+        al.add("Orange");
+        al.add("Pink");
+        al.add("Gray");
+        al.add("Black");
+        al.add("Yellow");
+        al.add("Green");
+        al.add("Blue");
+        al.add("Brown");
+        al.add("White");
+
+        for(String str : al){
+            System.out.println("FULL LIST = " + str + " " + ml.containsKey(str));
+            int count = ml.containsKey(str)? ml.get(str):0;
+            ml.put(str,++count);
+
+        }
+        for(String s : ml.keySet()){
+
+           Integer value = ml.get(s);
+            System.out.println("Counter " + s + " = " + value);
+
+        }
 
     }
 }
