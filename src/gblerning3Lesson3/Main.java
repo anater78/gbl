fdbdade6f58e7fed90cea49618c290dda3bac657
anater <anater78@gmail.com>
2019-11-20 10:55:53 +0300

    add lesson 3 NIO STUB

---
+++ src/gblerning3Lesson3/Main.java
@@ -0,0 +1,14 @@
+package gblerning3Lesson3;
+
+import gblerningLesson1Task1.Cat;
+import gblerningLesson1Task1.Men;
+import gblerningLesson1Task1.Robot;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("STUB");
+
+
+    }
+}
