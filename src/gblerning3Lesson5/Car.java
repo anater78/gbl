7028f32522647e76a164c2a2bd43c1efe4e71999
anater <anater78@gmail.com>
2019-11-30 15:50:49 +0300

    add lesson 5 Thread 2 add semaphor

--- src/gblerning3Lesson5/Car.java
+++ src/gblerning3Lesson5/Car.java
@@ -1,10 +1,14 @@
 package gblerning3Lesson5;
 
+import java.util.concurrent.Semaphore;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private static Semaphore smp;
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
@@ -22,13 +26,21 @@ public class Car implements Runnable {
     }
     @Override
     public void run() {
+        smp = new Semaphore(CARS_COUNT);
         try {
+
+            smp.acquire();
+
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
         } catch (Exception e) {
             e.printStackTrace();
+        }finally {
+            smp.release();
         }
+
+
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
